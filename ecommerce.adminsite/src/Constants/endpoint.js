327cef4307d1028d51ce507ae8e841040fd205f5
tuankiet1709 <tranhatuankiet1709@gmail.com>
2022-03-08 16:46:28 +0700

    create admin site with react

---
+++ ecommerce.adminsite/src/Constants/endpoint.js
@@ -0,0 +1,7 @@
+const Endpoints = {
+    authorize: 'api/authorize',
+    me: 'api/authorize/me',
+
+};
+
+export default Endpoints;
---
+++ ecommerce.adminsite/src/Constants/oidc-config.js
@@ -0,0 +1,3 @@
+export const UrlBackend = process.env.REACT_APP_BACKEND_URL;
+
+export const URL = process.env.REACT_APP_URL ?? "http://localhost:3000";
